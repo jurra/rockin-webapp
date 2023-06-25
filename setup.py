bca8252b00a64e0135d58302600e75963dac5a8a
jurra <jose.urra86@gmail.com>
2023-06-26 00:40:32 +0200

    Proper setup to get the pydantic data model dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
+from git import Repo
+import os.path
+
+# Clone data model from pydantic as a submodule
+if not os.path.exists("datamodel"):
+    repo_url = "git@github.com:jurra/rockin-datamodel.git"
+    Repo.clone_from(repo_url, "datamodel")
+#     repo = Repo("rockin-datamodel")
+#     os.rename("rockin-datamodel", "pydantic_models")
 
 setup(name='rockin',
       version='0.1',
