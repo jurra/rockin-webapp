a6f12791779e2d1a5265d4dcda8de4dd4e6cb322
jurra <jose.urra86@gmail.com>
2023-07-03 21:49:12 +0200

    AC: Setup datetime format and correct timezone

--- rockin/settings.py
+++ rockin/settings.py
@@ -123,11 +123,23 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
+
+DATE_FORMAT = 'Y-m-d'
+TIME_FORMAT = 'H:i:s'
+DATETIME_FORMAT = 'Y-m-d H:i:s'
+
+
+TIME_INPUT_FORMATS = [
+    '%Y-%m-%d:%H:%M:%S',
+]
+
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_L10N = False
 
 
 # Static files (CSS, JavaScript, Images)
